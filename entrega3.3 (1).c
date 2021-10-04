a0f883161e579faec2bf7218d4a318ffdb35be70
marccuadrado <91879314+marccuadrado@users.noreply.github.com>
2021-10-04 20:44:24 +0200

    Add files via upload

---
+++ entrega3.3 (1).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int GuanyadorData {partidas, resultado, nombre)
+	i=0;
+
+	while(i<lista.num)
+		if(strcpm (lista partida[i].fecha,fecha==0;
+			sprintf
+				return lista.persona[i].nombre
+
+		i++;
+	
+	return 0;
+}
+
