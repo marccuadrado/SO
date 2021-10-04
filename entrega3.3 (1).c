b5c644e907ba6645a79c898153e66d7e6c604356
marccuadrado <91879314+marccuadrado@users.noreply.github.com>
2021-10-04 20:51:26 +0200

    Update entrega3.3 (1).c

--- entrega3.3 (1).c
+++ entrega3.3 (1).c
@@ -4,12 +4,9 @@ int GuanyadorData {partidas, resultado, nombre)
 	i=0;
 
 	while(i<lista.num)
-		if(strcpm (lista partida[i].fecha,fecha==0;
-			sprintf
-				return lista.persona[i].nombre
-
+		if(strcpm (lista partida [i].fecha,fecha==0;
+			sprintf(nombre, %s %s, nombre, lista.jugador[i].nombre
+				
 		i++;
-	
-	return 0;
 }
 
