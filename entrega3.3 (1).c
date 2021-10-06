869e330efee161bed434221db664a1f11a70804a
marccuadrado <91879314+marccuadrado@users.noreply.github.com>
2021-10-06 18:45:26 +0200

    Update entrega3.3 (1).c

--- entrega3.3 (1).c
+++ entrega3.3 (1).c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int GuanyadorData {partidas, resultado, nombre)
-	i=0;
+SELECT partidas.ganador from (partidas) 	(guanyador data)
+WHERE partidas.fecha = '2020-12-11'
+	
 
-	while(i<lista.num)
-		if(strcpm (lista partida [i].fecha,fecha==0;
-			sprintf(nombre, %s %s, nombre, lista.jugador[i].nombre
-				
-		i++;
+SELECT distincs jugador.nombre from (partidas)	(nom guanyar + de 30p)
+WHERE partida.puntos >= 30;
+AND jugador.edad >= 18;
+	
+
+Select jugador.nombre FROM (jugador, partida)		(nom amb + punts)
+WHERE partida.puntos = select MAX(partida.puntos) FROM (partida)
+AND jugador.id = partidas.id
+
 }
 
